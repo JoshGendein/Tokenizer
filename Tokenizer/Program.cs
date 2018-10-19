3c547e90efecfd931d1233aa9f6219cdcb0628fe
Josh <joshua@gendein.com>
2018-10-19 08:05:53 -0700

    Restructured. Moved more responsibility into TokenHandler.

--- Tokenizer/Program.cs
+++ Tokenizer/Program.cs
@@ -10,13 +10,14 @@ namespace Tokenizer
     public class Program
     {
         private static List<String> documents;
-        private static int maxThreads = Environment.ProcessorCount;
+        private static readonly int maxThreads = Environment.ProcessorCount;
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a directory relative to this program:");
             var inputDirectory = Console.ReadLine();
             var directory = System.Environment.CurrentDirectory + inputDirectory;
             Console.WriteLine(directory);
+
             if(!CheckDirectory(directory))
             {
                 return;
@@ -25,22 +26,8 @@ namespace Tokenizer
             //Gather a list of file paths from directory.
             documents = Directory.EnumerateFiles(directory, "*.txt").ToList();
 
-            var threads = new Thread[maxThreads];
-
-            for(int i=0; i < maxThreads; i++)
-            {
-                var thisHandlersDocumentSet = DivideDocuments(i);
-                var handler = new TokenHandler(thisHandlersDocumentSet);
-                var handlerThread = new Thread(new ThreadStart(handler.tokenize));
-                threads[i] = handlerThread;
-                handlerThread.Start();
-            }
-
-            foreach(var thread in threads)
-            {
-                thread.Join();
-            }
-
+            var handler = new TokenHandler(documents, maxThreads);
+            handler.Run();
         }
         public static bool CheckDirectory(String directory)
         {
@@ -50,22 +37,6 @@ namespace Tokenizer
                 return false;
             }
             return true;
-        }
-
-        //Takes the list of all documents and divides them into smaller lists so each thread can access them.
-        public static List<String> DivideDocuments(int index)
-        {
-            var finalSet = new List<String>();
-
-            for(int i=0; i < documents.Count; i++)
-            {
-                if((i % maxThreads) == index)
-                {
-                    finalSet.Add(documents[i]);
-                }
-            }
-
-            return finalSet;
         }
     }
 }
--- Tokenizer/src/TokenHandler.cs
+++ Tokenizer/src/TokenHandler.cs
@@ -1,22 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 
 namespace Tokenizer.src
 {
     public class TokenHandler
     {
-        private List<String> files { get; set; }
-        public TokenHandler(List<String> files)
+        private List<String> Documents { get; set; }
+        private Thread[] threads;
+        public TokenHandler(List<String> documents, int threadCount)
         {
-            this.files = files;
+            this.Documents = documents;
+            this.threads = new Thread[threadCount];
+
+            //Establish DB connection.
+
         }
 
-        public void tokenize()
+        public void Run()
+        {
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thisHandlersDocumentSet = DivideDocuments(i);
+                var handlerThread = new Thread(new ThreadStart(this.Tokenize));
+                threads[i] = handlerThread;
+                handlerThread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private void Tokenize()
         {
             
         }
 
-        
+        //Takes the list of all documents and divides them into smaller lists so each thread can access their individual set.
+        private List<String> DivideDocuments(int index)
+        {
+            var finalSet = new List<String>();
+
+            for (int i = 0; i < Documents.Count; i++)
+            {
+                if ((i % threads.Length) == index)
+                {
+                    finalSet.Add(Documents[i]);
+                }
+            }
+
+            return finalSet;
+        }
+
+
     }
 }
